93bd8ff7a9389d534fe1e4c87005e0b399f87eeb
sutaojie <sutaojie@gmail.com>
2018-07-29 22:21:06 +0800

    下拉加载数据

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,15 +4,35 @@ import './index.css'
 import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
+import { InfiniteScroll } from 'mint-ui';
+
+Vue.use(InfiniteScroll);
 let app = new Vue({
   el:'#app',
   data:{
-    lists:null
+    lists:null,
+    pageNum:1,
+    pageSize:3,
+    loading:false,
+    allLoaded:false
   },
   created(){
-    axios.get(url.hotLists,{pageNum:1, pageSize:6})
+    this.getLists()
+  },
+  methods:{
+    getLists(){
+      if(this.allLoaded) return
+      this.loading = true
+      axios.get(url.hotLists,{pageNum:this.pageNum, pageSize:this.pageSize})
       .then((res)=>{
-          this.lists = res.data.lists
+          let curLists = res.data.lists
+          // 数据是否加载完毕
+          this.allLoaded = this.pageSize > curLists.length ? true: false
+          // 获取数据
+          this.lists = this.lists ? this.lists.concat(curLists): curLists
+          this.loading = false
+          this.pageSize += 1
       })
+    }
   }
 })
