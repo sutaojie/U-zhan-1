760ab2d37bffc0055b3f0cea195eab514e61f0dd
sutaojie <sutaojie@gmail.com>
2018-08-07 19:18:25 +0800

    重新启用

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -2,7 +2,7 @@ let url={
     hotLists:'/index/hotLists'
 }
 // 开发环境
-let host = 'http://rapapi.org/mockjsdata/31797' 
+let host = 'http://rap2api.taobao.org/app/mock/7058' 
 // 真实环境
 //let host
 for (const key in url) {
